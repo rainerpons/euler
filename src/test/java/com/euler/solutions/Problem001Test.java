61d67ea9638d3254da005e285bfcb13f2baf7f4f
Rainer Pons <rpons@ucsc.edu>
2018-08-18 20:34:53 -0700

    Create unit test class
    Completed an exhaustive unit test class for methods listOfMultiples()
    and sumOfListOfMultiples() from the Problem001 class. Since the
    solution() method is completely dependent on sumOfListOfMultiples(),
    which has been thoroughly tested, then it can be assumed that solution()
    is correct

--- src/test/java/com/euler/solutions/Problem001Test.java
+++ src/test/java/com/euler/solutions/Problem001Test.java
@@ -5,135 +5,235 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static com.euler.solutions.Problem001.listOfMultiples;
+import static com.euler.solutions.Problem001.sumOfListOfMultiples;
 
 @SuppressWarnings("unused")
 public class Problem001Test {
+	private int multipleA;
+	private int multipleB;
+	private int limit;
+
 	/*
 	 * The following nine methods test listOfMultiples() from the Problem001 class.
 	 */
-	@Test
-	public void should_returnMultiples_when_argumentsArePositive() {
-		// given
-		int multipleA = 3;
-		int multipleB = 5;
-		int limit = 10;
 
-		// when
+	@Test
+	public void should_returnMultiples_when_allArgumentsArePositive() {
+		setMultiplesPositive();
+		setLimitPositive();
+
 		List<Integer> expected = Arrays.asList(3, 5, 6, 9);
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 
-		// then
-		Assert.assertEquals(expected, actual);
-	}
-
-	@Test
-	public void should_returnEmptyList_when_multiplesArePositiveAndLimitIsNegative() {
-		// given
-		int multipleA = 3;
-		int multipleB = 5;
-		int limit = -10;
-
-		// when
-		List<Integer> expected = new ArrayList<>();
-		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
-
-		// then
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void should_returnEmptyList_when_multiplesArePositiveAndLimitIsZero() {
-		// given
-		int multipleA = 3;
-		int multipleB = 5;
-		int limit = 0;
+		setMultiplesPositive();
+		setLimitZero();
 
-		// when
 		List<Integer> expected = new ArrayList<>();
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 
-		// then
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void should_returnEmptyList_when_multiplesArePositiveAndLimitIsNegative() {
+		setMultiplesPositive();
+		setLimitNegative();
+
+		List<Integer> expected = new ArrayList<>();
+		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
+
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsNegative() {
-		// given
-		int multipleA = 0;
-		int multipleB = 5;
-		int limit = -10;
+	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsPositive_caseA() {
+		setEitherMultipleZero();
+		setLimitPositive();
 
-		// when and then
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsZero() {
-		// given
-		int multipleA = 0;
-		int multipleB = 5;
-		int limit = 0;
+	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsZero_caseA() {
+		setEitherMultipleZero();
+		setLimitZero();
 
-		// when and then
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsPositive() {
-		// given
-		int multipleA = 0;
-		int multipleB = 5;
-		int limit = 10;
+	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsNegative_caseA() {
+		setEitherMultipleZero();
+		setLimitNegative();
 
-		// when and then
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 	}
 
 	@Test
 	public void should_returnEmptyList_when_argumentsAreNegative() {
-		// given
-		int multipleA = -3;
-		int multipleB = -5;
-		int limit = -10;
+		setEitherMultipleNegative();
+		setLimitNegative();
 
-		// when
 		List<Integer> expected = new ArrayList<>();
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 
-		// then
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void should_returnEmptyList_when_eitherMultipleIsNegativeAndLimitIsZero() {
-		// given
-		int multipleA = -3;
-		int multipleB = 5;
-		int limit = 0;
+		setEitherMultipleNegative();
+		setLimitZero();
 
-		// when
 		List<Integer> expected = new ArrayList<>();
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 
-		// then
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void should_returnMultiples_when_eitherMultipleIsNegativeAndLimitIsPositive() {
-		// given
-		int multipleA = -3;
-		int multipleB = 5;
-		int limit = 10;
+		setEitherMultipleNegative();
+		setLimitPositive();
 
-		// when
 		List<Integer> expected = Arrays.asList(3, 5, 6, 9);
 		List<Integer> actual = listOfMultiples(multipleA, multipleB, limit);
 
-		// then
 		Assert.assertEquals(expected, actual);
+	}
+
+	/*
+	 * The following nine methods test sumOfListOfMultiples() from the Problem001 class.
+	 */
+
+	@Test
+	public void should_returnSum_when_allArgumentsArePositive() {
+		setMultiplesPositive();
+		setLimitPositive();
+
+		Integer expected = 23;
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void should_returnZero_when_multiplesArePositiveAndLimitIsZero() {
+		setMultiplesPositive();
+		setLimitZero();
+
+		Integer expected = 0;
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void should_returnZero_when_multiplesArePositiveAndLimitIsNegative() {
+		setMultiplesPositive();
+		setLimitNegative();
+
+		Integer expected = 0;
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsPositive_caseB() {
+		setEitherMultipleZero();
+		setLimitPositive();
+
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsZero_caseB() {
+		setEitherMultipleZero();
+		setLimitZero();
+
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void should_throwException_when_eitherMultipleIsZeroAndLimitIsNegative_caseB() {
+		setEitherMultipleZero();
+		setLimitNegative();
+
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+	}
+
+	@Test
+	public void should_returnSum_when_eitherMultipleIsNegativeAndLimitIsPositive() {
+		setEitherMultipleNegative();
+		setLimitPositive();
+
+		Integer expected = 23;
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void should_returnZero_when_eitherMultipleIsNegativeAndLimitIsZero() {
+		setEitherMultipleNegative();
+		setLimitZero();
+
+		Integer expected = 0;
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void should_returnZero_when_eitherMultipleIsNegativeAndLimitIsNegative() {
+		setEitherMultipleNegative();
+		setLimitNegative();
+
+		Integer expected = 0;
+		Integer actual = sumOfListOfMultiples(multipleA, multipleB, limit);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	/*
+	 * The following are helper methods for testing the listOfMultiples() and sumOfListOfMultiples()
+	 * methods from the Problem001 class.
+	 */
+
+	private void setMultiplesPositive() {
+		multipleA = 3;
+		multipleB = 5;
+	}
+
+	private void setEitherMultipleZero() {
+		multipleA = 0;
+		multipleB = 5;
+	}
+
+	private void setEitherMultipleNegative() {
+		multipleA = -3;
+		multipleB = 5;
+	}
+
+	private void setLimitPositive() {
+		limit = 10;
+	}
+
+	private void setLimitZero() {
+		limit = 0;
+	}
+
+	private void setLimitNegative() {
+		limit = -10;
 	}
 }
